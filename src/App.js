0d20a24efcc3e3385c6d0f5a18847cbe09227406
mohitsharma <gauravshrama68@gmail.com>
2021-11-13 17:26:03 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ width: "100%", height: "100%" }}>
+      <div
+        style={{
+          position: "absolute",
+          top: "0px",
+          left: "0px",
+          minHeight: "34px",
+          backgroundColor: "red",
+          right: "0px",
+        }}
+      ></div>
+      <div
+        style={{
+          position: "absolute",
+          left: "0px",
+          top: "34px",
+          bottom: "0px",
+          zIndex: 103,
+          // padding: 5,
+          backgroundColor: "orange",
+        }}
+      >
+        <div
+          style={{
+            width: "200px",
+            height: "100%",
+            padding: "0px",
+            float: "left",
+            margin: "0px",
+            position: "relative",
+            zIndex: 1,
+            overflow: "auto",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h1>fdds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fds</h1>
+          <h1>fdsd</h1>
+        </div>
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          left: "200px",
+          top: "34px",
+          bottom: "0px",
+          right: "0px",
+          zIndex: 103,
+          // padding: 5,
+        }}
+      >
+        <div
+          style={{
+            // wid  th: "500px",
+            height: "100%",
+            padding: "0px",
+            right: "0px",
+            margin: "0px",
+            position: "relative",
+            overflow: "auto",
+            backgroundColor: "blue",
+          }}
+        >
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsf</h1>
+          <h1>fdsfd</h1>
+        </div>
+      </div>
     </div>
   );
 }
